1dacd00d239d7fda0184f7218ec1fe86fab56d33
Sarvesh Kumar Sharma <shsarv@gmail.com>
2020-09-14 22:03:39 +0530

    Create pointers_in_C.c

---
+++ pointers_in_C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int s,r;
+    s=*a;
+    *a= *a + *b;
+    *b= s> *b? s- *b : *b - s;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
